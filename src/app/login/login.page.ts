3eb772e2a13acba84c51500398fa7af0b5b402fd
Franchi <franchi@franchi-hp-laptop.home>
2020-01-05 17:52:41 +0100

    login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-
+import{LoginService}from './loginservice/login.service'
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -8,12 +8,18 @@ import { NavController } from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
   model : any={}
-  constructor(public navCtrl : NavController) { }
+  email
+  password
+  constructor(public navCtrl : NavController,private loginservice:LoginService) { }
 
   ngOnInit() {
   }
 inscription() {
 this.navCtrl.navigateRoot('/inscription')
 }
-
+login(){
+this.loginservice.login(this.email,this.password).subscribe(data=>{
+  this.navCtrl.navigateRoot('/home/tab1')
+})
+}
 }
--- src/app/login/loginservice/login.service.ts
+++ src/app/login/loginservice/login.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@angular/core'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 
 
 @Injectable({
     providedIn: 'root',
 })
 export class LoginService {
-  
+    apiUrl = 'http://127.0.0.1:8000/api/login'
+    header: HttpHeaders
+    constructor(private _http: HttpClient) {
+      this.header = new HttpHeaders;
+      this.header =  this.header.append('Content-type','application/json');
+      this.header =  this.header.append('Accept','application/json');
+    }
+    login(email , password){
+        return this._http.post(this.apiUrl,{email,password},{headers : this.header})
+    }
+
    
 }
